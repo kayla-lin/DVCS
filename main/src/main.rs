3d0481656c5b74bc1bcb572130aa1d89c0b3a9b9
idqam <owenjave5@gmail.com>
2022-12-11 20:10:05 -0500

    status tests

--- main/src/main.rs
+++ main/src/main.rs
@@ -84,6 +84,8 @@ fn main() {
 
 
 mod acceptance_tests{
+    use std::panic;
+
     use staging::staging_storage::Staging;
     use usemods::user_interaction;
 
@@ -135,5 +137,32 @@ mod acceptance_tests{
         std::fs::create_dir_all("/tmp/dvcs_testi/");
         let add_res = user_interaction::add_in(file_path);
         assert_eq!(add_res, false);
+    }
+
+    //level 3 status, heads, cat, log
+    #[test]
+    fn status_test_3a(){
+        //pass
+        const DVCS_HIDDEN: &str = "/tmp/dvcs_team";
+        let file_path = String::from("/tmp/dvcs_testi/");
+        
+
+        std::fs::create_dir_all(DVCS_HIDDEN);
+        std::fs::create_dir_all("/tmp/dvcs_testi/");
+        
+        let status_res = panic::catch_unwind(|| {user_interaction::status_in(file_path)});
+        assert_eq!(status_res.unwrap_or(true), true);
+    }
+
+    #[test]
+    fn status_test_3b(){
+        //fails
+        const DVCS_HIDDEN: &str = "/tmp/dvcs_team";
+        let file_path = String::from("/tmp//");
+
+        std::fs::create_dir_all(DVCS_HIDDEN);
+        std::fs::create_dir_all("/tmp/dvcs_testi/");
+        let status_res = panic::catch_unwind(|| {user_interaction::status_in(file_path)});
+        assert_eq!(status_res.unwrap_or(false), false);
     }
 }
