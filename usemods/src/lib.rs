cfb41d35fd654913c928b6ee5f94d2264275e57d
Kayla Lin <kaylalin@Kaylas-MacBook-Pro.local>
2022-12-11 20:10:42 -0500

    working on find hidden folder

--- usemods/src/lib.rs
+++ usemods/src/lib.rs
@@ -1,75 +1,139 @@
-pub mod user_feedback{
+pub mod user_feedback {
 
     use colored::Colorize;
     use rand::prelude::*;
     use std::io;
     use std::io::prelude::*;
-    
-    pub fn display_all_errors(errors: Vec<String>) -> bool {
-            //errors to iterator
-            let error_inter: Vec<String> =  errors.into_iter().collect();
-            let mut rng = rand::thread_rng();
-            
-            //display errors
-            error_inter.iter().for_each(|error:&String| println!("{}", error.truecolor(rng.gen(), rng.gen(), rng.gen())));
-            return true;
-        }
-    
-    pub fn display_first_error(errors: Vec<String>) -> bool {
-            println!("{}", errors[0].red());
-            return true;
-        }
-        
-        
-    pub fn format_error_alt(errors: Vec<String>) -> bool{
-            //print every 3 errors and display more when user enters
-            fn pause() {
-                let mut stdin = io::stdin();
-                let mut stdout = io::stdout();
-            
-                write!(stdout, "Press any key to continue...").unwrap();
-                stdout.flush().unwrap();
-            
-                // Read a single byte and discard
-                let _ = stdin.read(&mut [0u8]).unwrap();
-            }
 
-            let  mut rng = rand::thread_rng();
-        
-            errors.chunks(3).for_each(|error| {
-                
-                error.to_vec().iter().for_each(|error| println!("{}", error.truecolor(rng.gen(), rng.gen(), rng.gen())));
-                pause();
-            });
-            return true;
-    
-        }
-    
-    
+    pub fn display_all_errors(errors: Vec<String>) -> bool {
+        //errors to iterator
+        let error_inter: Vec<String> = errors.into_iter().collect();
+        let mut rng = rand::thread_rng();
+
+        //display errors
+        error_inter.iter().for_each(|error: &String| {
+            println!("{}", error.truecolor(rng.gen(), rng.gen(), rng.gen()))
+        });
+        return true;
     }
 
+    pub fn display_first_error(errors: Vec<String>) -> bool {
+        println!("{}", errors[0].red());
+        return true;
+    }
 
-pub mod user_interaction{
+    pub fn format_error_alt(errors: Vec<String>) -> bool {
+        //print every 3 errors and display more when user enters
+        fn pause() {
+            let mut stdin = io::stdin();
+            let mut stdout = io::stdout();
 
+            write!(stdout, "Press any key to continue...").unwrap();
+            stdout.flush().unwrap();
+
+            // Read a single byte and discard
+            let _ = stdin.read(&mut [0u8]).unwrap();
+        }
+
+        let mut rng = rand::thread_rng();
+
+        errors.chunks(3).for_each(|error| {
+            error
+                .to_vec()
+                .iter()
+                .for_each(|error| println!("{}", error.truecolor(rng.gen(), rng.gen(), rng.gen())));
+            pause();
+        });
+        return true;
+    }
+}
+
+pub mod user_interaction {
+
+    use crate::user_feedback::{display_all_errors, display_first_error, format_error_alt};
     use stager;
-    use std::{path::Path, collections::HashMap};
-    use crate::user_feedback::{display_first_error, display_all_errors, format_error_alt};
-    use storage_hiding::repository_storage::{RepositoryStorage, self}; 
-    
+    use std::path::{Path, PathBuf};
+    use std::{collections::HashMap, fs};
+    use storage_hiding::repository_storage::{self, RepositoryStorage};
+
+    /// Finds the hidden dvcs folder based on the file that the user gives
+    /// Traverses 40 layers up
+    pub fn find_hidden_dvcs_folder(current_path: String) -> Result<PathBuf, String> {
+        let mut found: Option<PathBuf> = None;
+        let mut found_err = false;
+        match Path::new(&current_path).try_exists() {
+            Ok(_) => {
+                let mut parent: Option<&String> = Some(
+                    Path::new(&current_path)
+                        .into_os_string()
+                        .into_string()
+                        .unwrap(),
+                );
+                for x in 0..40 {
+                    match fs::read_dir(&current_path) {
+                        Ok(file_entry) => {
+                            // * Loop through each entry in the directory
+                            let mut found_parent: Option<&String> =
+                                file_entry.into_iter().fold(None, |acc, entry| {
+                                    if let Ok(path) = entry {
+                                        let path_buffer = path.path();
+
+                                        if let Ok(attributes) = fs::metadata(path_buffer.clone()) {
+                                            if attributes.is_dir() {
+                                                println!("{:?}", path_buffer);
+                                                return Some(
+                                                    &path_buffer
+                                                        .into_os_string()
+                                                        .into_string()
+                                                        .unwrap(),
+                                                );
+                                                // if path_buffer.file_name() == ".dvcs_hidden" {
+                                                //     found = Some(path_buffer);
+                                                // }
+                                            } else {
+                                                return acc;
+                                            }
+                                        } else {
+                                            return None;
+                                        }
+
+                                        //let file_path = path_buffer.display().to_string();
+                                        // * Loops through all files, not the DVCS hidden one
+                                        // if !file_path.contains(".dvcs_hidden") {}
+                                    } else {
+                                        return None;
+                                    }
+                                });
+                            found_parent = parent;
+                        }
+                        Err(_) => {
+                            found_err = true;
+                        }
+                    };
+                    if (found.is_some() || found_err) {
+                        break;
+                    }
+                }
+            }
+            Err(_) => todo!(),
+        }
+        return Err("Could not find repository from file path".to_string());
+    }
+
     pub fn init_in(file_path: String) -> bool {
-        //let res = Path::new(&file_path).try_exists().unwrap_or_else(|_| false); 
+        //let res = Path::new(&file_path).try_exists().unwrap_or_else(|_| false);
         match Path::new(&file_path).try_exists().unwrap_or_else(|_| false) {
             true => {
                 let fp = file_path.clone();
                 let init_res = stager::stager::Stager::init(file_path);
-                if init_res.is_err(){
-                    let t_er = init_res.unwrap_err();
+                if init_res.is_err() {
+                    let t_er = "file path: ".to_owned() + &fp.to_string() + " does not exists!";
                     display_first_error(vec![t_er]);
                     return false;
                 }
                 println!("Initialized empty dvcs repository in {}", &fp);
                 return true;
-            },
+            }
             false => {
                 println!("Error!");
                 println!("Which error function do you want to use?");
@@ -79,7 +143,7 @@ pub mod user_interaction{
                 println!("\n1. Display first error\n2. Display all errors\n3. Display errors in chunks\n");
                 let mut input = String::new();
                 let errfn = errchoice.read_line(&mut input).unwrap();
-        
+
                 let t_er = "file path: ".to_owned() + &file_path.to_string() + " does not exists!";
                 match errfn {
                     1 => {
@@ -99,80 +163,74 @@ pub mod user_interaction{
                     }
                     _ => {
                         //display first error
-                    display_all_errors(vec![t_er]);
-                    return false;
-                    },
-                }
-            },
-        }
-    }
-
-
-    pub fn diff_in(file_path: String, head: String) -> bool{
-        let res: bool = Path::new(&file_path).try_exists().unwrap_or_else(|_| false);
-        match res {
-            true => {
-                println!("File exists, diffing...");
-                let stager = stager::stager::Stager{};
-                let diff_res = stager.diff(file_path, head);
-                if diff_res.is_err(){
-                    let t_er = diff_res.unwrap_err();
-                    display_first_error(vec![t_er]);
-                    return false;
-                }
-                return true;
-            },
-            false => {
-                println!("Error!");
-                println!("Which error function do you want to use?");
-                //read console input
-                //match input to error function
-
-                let errchoice = std::io::stdin();
-                println!("\n1. Display first error\n2. Display all errors\n3. Display errors in chunks\n");
-                let mut input = String::new();
-                let errfn = errchoice.read_line(&mut input).unwrap();
-        
-                let t_er = "file path: ".to_owned() + &file_path.to_string() + " does not exists!";
-                match errfn {
-                    1 => {
-                        //display first error
-                        display_first_error(vec![t_er]);
-                        return false;
-                    }
-                    2 => {
-                        //display all errors
                         display_all_errors(vec![t_er]);
                         return false;
                     }
-                    3 => {
-                        //display errors in chunks
-                        format_error_alt(vec![t_er]);
-                        return false;
-                    }
-                    _ => {
-                        //display first error
-                    display_all_errors(vec![t_er]);
-                    return false;
-                    },
                 }
             }
         }
     }
-    pub fn status_in(file_path: String) -> bool{
+
+    pub fn diff_in(file_path: String, head: String) -> bool {
+        let res: bool = Path::new(&file_path).try_exists().unwrap_or_else(|_| false);
+        match res {
+            true => {
+                println!("File exists, diffing...");
+                let stager = stager::stager::Stager {};
+                stager.diff(file_path, head);
+                return true;
+            }
+            false => {
+                println!("Error!");
+                println!("Which error function do you want to use?");
+                //read console input
+                //match input to error function
+
+                let errchoice = std::io::stdin();
+                println!("\n1. Display first error\n2. Display all errors\n3. Display errors in chunks\n");
+                let mut input = String::new();
+                let errfn = errchoice.read_line(&mut input).unwrap();
+
+                let t_er = "file path: ".to_owned() + &file_path.to_string() + " does not exists!";
+                match errfn {
+                    1 => {
+                        //display first error
+                        display_first_error(vec![t_er]);
+                        return false;
+                    }
+                    2 => {
+                        //display all errors
+                        display_all_errors(vec![t_er]);
+                        return false;
+                    }
+                    3 => {
+                        //display errors in chunks
+                        format_error_alt(vec![t_er]);
+                        return false;
+                    }
+                    _ => {
+                        //display first error
+                        display_all_errors(vec![t_er]);
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    pub fn status_in(file_path: String) -> bool {
         let res: bool = Path::new(&file_path).try_exists().unwrap_or_else(|_| false);
         match res {
             true => {
                 println!("File exists, status...");
                 let status_res = stager::stager::Stager::status(file_path);
 
-                if status_res.is_ok(){
+                if status_res.is_ok() {
                     return true;
-                }else {
+                } else {
                     println!("Error! File path empty");
                     return false;
                 }
-            },
+            }
             false => {
                 println!("Error!");
                 println!("Which error function do you want to use?");
@@ -183,7 +241,7 @@ pub mod user_interaction{
                 println!("\n1. Display first error\n2. Display all errors\n3. Display errors in chunks\n");
                 let mut input = String::new();
                 let errfn = errchoice.read_line(&mut input).unwrap();
-        
+
                 let t_er = "file path: ".to_owned() + &file_path.to_string() + " does not exists!";
                 match errfn {
                     1 => {
@@ -203,42 +261,37 @@ pub mod user_interaction{
                     }
                     _ => {
                         //display first error
-                    display_all_errors(vec![t_er]);
-                    return false;
-                    },
+                        display_all_errors(vec![t_er]);
+                        return false;
+                    }
                 }
             }
         }
-
     }
 
-    pub fn remove_in(file_path: String) -> bool{    
+    pub fn remove_in(file_path: String) -> bool {
         return stager::stager::Stager::remove(file_path).is_ok();
     }
 
-    pub fn add_in(file_path: String) -> bool{
+    pub fn add_in(file_path: String) -> bool {
         let res = stager::stager::Stager::add(file_path);
         return res.is_ok();
     }
 
-    pub fn see_diff_in(snapshot: &HashMap<String, String>) -> (HashMap<String, String>, bool){
+    pub fn see_diff_in(snapshot: &HashMap<String, String>) -> (HashMap<String, String>, bool) {
         //new instance of RepositoryStorage
         let repo = RepositoryStorage::new();
         (repo.see_diff(snapshot), true)
     }
-    
-
-
-
 }
 
-pub mod dir_c{
-    use std::{fs, io};
+pub mod dir_c {
     use std::fs::File;
+    use std::{fs, io};
 
     pub fn create_dir_main() -> io::Result<()> {
         const DVCS_HIDDEN: &str = "/tmp/dvcs_team";
-   
+
         fs::create_dir(DVCS_HIDDEN)?;
         fs::create_dir("/tmp/dvcs_testi/")?;
         let file = File::create("/tmp/dvcs_testi/");
@@ -246,53 +299,53 @@ pub mod dir_c{
     }
 }
 
-mod err_handling_tests{
+mod err_handling_tests {
     use crate::user_feedback::*;
 
     #[test]
-    fn test_display_first_error(){
+    fn test_display_first_error() {
         let test_err = vec!["test error on first err fn".to_string()];
         let res = display_first_error(test_err);
         assert_eq!(res, true);
     }
     #[test]
-    fn test_display_all_errors(){
+    fn test_display_all_errors() {
         let test_err = vec!["test error on display all err fn".to_string()];
         let res = display_all_errors(test_err);
         assert_eq!(res, true);
     }
     #[test]
-    fn test_format_error_alt(){
-        print!("Press any key to continue...\n"); 
+    fn test_format_error_alt() {
+        print!("Press any key to continue...\n");
         let test_err = vec!["test error on format err fn".to_string()];
         let res = format_error_alt(test_err);
         assert_eq!(res, true);
     }
-
-
 }
 
-mod user_interaction_tests{
-    use crate::user_interaction::diff_in;
-    use crate::user_interaction::init_in;
-    use crate::user_interaction::status_in;
-    use crate::user_interaction::remove_in;
+mod user_interaction_tests {
     use crate::user_interaction::add_in;
-    
-    
+    use crate::user_interaction::diff_in;
+    use crate::user_interaction::find_hidden_dvcs_folder;
+    use crate::user_interaction::init_in;
+    use crate::user_interaction::remove_in;
+    use crate::user_interaction::status_in;
 
-    
+    #[test]
+    fn find_hidden_dvcs_folder_test() {
+        find_hidden_dvcs_folder("./".to_string());
+    }
+
     static mut TEST_PATH: &str = "/tmp/dvcs_test/";
     #[test]
-    fn init_test_succ(){
+    fn init_test_succ() {
         //create a valid path
         let test_path = "/tmp/dvcs_test/";
         let res = init_in(test_path.to_string());
         assert_eq!(res, true);
-        
     }
     #[test]
-    fn init_test_fail(){
+    fn init_test_fail() {
         //create an invalid path
         let test_path = "/tmp/dvcs_st";
         let res = init_in(test_path.to_string());
@@ -300,32 +353,28 @@ mod user_interaction_tests{
     }
     #[test]
 
-    fn diff_in_test(){
+    fn diff_in_test() {
         let test_path = "/tmp/dvcs_test/";
         let res = diff_in(test_path.to_string(), "HEAD".to_string());
         assert_eq!(res, true);
     }
     #[test]
-    fn status_in_test(){
+    fn status_in_test() {
         let test_path = "/tmp/dvcs_test/";
         let res = status_in(test_path.to_string());
         assert_eq!(res, true);
     }
 
     #[test]
-    fn remove_in_test(){
+    fn remove_in_test() {
         let test_path = "/tmp/dvcs_test/";
         let res = remove_in(test_path.to_string());
         assert_eq!(res, true);
     }
     #[test]
-    fn add_in_test(){
+    fn add_in_test() {
         let test_path = "/tmp/dvcs_test/";
         let res = add_in(test_path.to_string());
         assert_eq!(res, true);
     }
-
-    
 }
-
-
